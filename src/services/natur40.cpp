4dd71f3a5dd507bb7b0be86e6d5728f59a996900
Christian Beilschmidt <beilschmidt@mathematik.uni-marburg.de>
2020-02-13 14:09:13 +0100

    adapted catalog details to changes

--- src/services/natur40.cpp
+++ src/services/natur40.cpp
@@ -1,119 +1,69 @@
-
 #include "services/httpservice.h"
 #include "userdb/userdb.h"
 #include "util/configuration.h"
 #include <util/curl.h>
 
 #include <jwt/jwt.hpp>
-#include <utility>
 
-/*
- * This class provides methods for Natur 4.0 users
+/**
+ * This class provides methods for Nature 4.0 users.
  *
- * Operations:
- * - request = login: login with credential, returns sessiontoken
- *   - parameters:
- *     - token
- *  - request = clientToken: get the client token for provider
- *  - request = sourcelist: list available sources from raster db
+ * Requests:
+ * - `login`: login with credential, returns sessiontoken
+ *      - parameters
+ *          - token
+ *  - `clientToken`: returns the client token for provider
+ *  - `sourcelist`: list available sources from the catalog
+ *      - parameters
+ *          - sessiontoken: Requires the user to be logged in
  */
 class Natur40Service : public HTTPService {
-public:
-    using HTTPService::HTTPService;
-
-    ~Natur40Service() override = default;
-
-private:
-    class CatalogEntry {
     public:
-        std::string id, name, description, url;
+        using HTTPService::HTTPService;
 
-        CatalogEntry(std::string id, std::string name, std::string description, std::string url) :
-                id(std::move(id)), name(std::move(name)), description(std::move(description)), url(std::move(url)) {}
+        ~Natur40Service() override = default;
 
-        auto toJson() const -> Json::Value;
-    };
+    private:
+        class CatalogEntry {
+            public:
+                std::string global_id;
+                std::string title;
+                std::string description;
+                std::string user_url;
+                std::string provider_type;
+                std::string provider_id;
+                std::string provider_url;
+                std::string dataset_type;
+                std::string dataset_id;
+                std::string dataset_url;
 
-    static constexpr const char *EXTERNAL_ID_PREFIX = "JWT:";
+                explicit CatalogEntry(const Json::Value &json) :
+                        global_id(json.get("global_id", "").asString()),
+                        title(json.get("title", "").asString()),
+                        description(json.get("description", "").asString()),
+                        user_url(json.get("user_url", "").asString()),
+                        provider_type(json.get("provider", Json::Value(Json::ValueType::arrayValue)).get("type", "").asString()),
+                        provider_id(json.get("provider", Json::Value(Json::ValueType::arrayValue)).get("id", "").asString()),
+                        provider_url(json.get("provider", Json::Value(Json::ValueType::arrayValue)).get("url", "").asString()),
+                        dataset_type(json.get("dataset", Json::Value(Json::ValueType::arrayValue)).get("type", "").asString()),
+                        dataset_id(json.get("dataset", Json::Value(Json::ValueType::arrayValue)).get("id", "").asString()),
+                        dataset_url(json.get("dataset", Json::Value(Json::ValueType::arrayValue)).get("url", "").asString()) {}
 
-    void run() override;
+                auto toJson() const -> Json::Value;
+        };
 
-    auto queryCatalog(const std::string &token) -> std::vector<CatalogEntry>;
+        static constexpr const char *EXTERNAL_ID_PREFIX = "JWT:";
 
-    auto createCatalogSession(const std::string &token) const -> std::string;
+        void run() override;
 
-    auto getCatalogJSON(const std::string &sessionId) const -> Json::Value;
+        auto queryCatalog(const std::string &token) -> std::vector<CatalogEntry>;
+
+        auto createCatalogSession(const std::string &token) const -> std::string;
+
+        auto getCatalogJSON(const std::string &sessionId) const -> Json::Value;
 };
 
-auto Natur40Service::CatalogEntry::toJson() const -> Json::Value {
-    Json::Value v(Json::ValueType::objectValue);
-    v["id"] = id;
-    v["name"] = name;
-    v["description"] = description;
-    v["url"] = url;
-    return v;
-}
-
-REGISTER_HTTP_SERVICE(Natur40Service, "natur40");
-
-
-auto Natur40Service::createCatalogSession(const std::string &token) const -> std::string {
-    cURL curl;
-    std::stringstream data;
-    curl.setOpt(CURLOPT_PROXY, Configuration::get<std::string>("proxy", "").c_str());
-
-    curl.setOpt(CURLOPT_URL, concat(Configuration::get<std::string>("natur40.catalog_auth_url"), token).c_str());
-    curl.setOpt(CURLOPT_WRITEFUNCTION, cURL::defaultWriteFunction);
-    curl.setOpt(CURLOPT_WRITEDATA, &data);
-    curl.setOpt(CURLOPT_COOKIEFILE, "");
-    curl.perform();
-
-
-    const std::vector<std::string> vector = curl.getCookies();
-
-    if (vector.empty()) {
-        throw NetworkException("Natur40 Catalog Cookie missing");
-    }
-
-    std::string cookie = vector[0];
-    return cookie.substr(cookie.rfind('\t') + 1);
-}
-
-auto Natur40Service::getCatalogJSON(const std::string &sessionId) const -> Json::Value {
-    cURL curl;
-    std::stringstream data;
-
-    curl.setOpt(CURLOPT_PROXY, Configuration::get<std::string>("proxy", "").c_str());
-
-    curl.setOpt(CURLOPT_URL, Configuration::get<std::string>("natur40.catalog_url").c_str());
-    curl.setOpt(CURLOPT_WRITEFUNCTION, cURL::defaultWriteFunction);
-    curl.setOpt(CURLOPT_COOKIE, concat("session=", sessionId, ";").c_str());
-    curl.setOpt(CURLOPT_WRITEDATA, &data);
-    curl.setOpt(CURLOPT_COOKIEFILE, "");
-    curl.perform();
-
-    Json::Reader reader(Json::Features::strictMode());
-    Json::Value entities;
-    if (!reader.parse(data.str(), entities))
-        throw std::runtime_error("Could not parse from Natur40 calatog");
-
-    return entities;
-}
-
-auto Natur40Service::queryCatalog(const std::string &token) -> std::vector<CatalogEntry> {
-    Json::Value json = getCatalogJSON(createCatalogSession(token));
-
-    std::vector<CatalogEntry> entries;
-
-    for (const Json::Value &v : json.get("entities", Json::Value(Json::ValueType::arrayValue))) {
-        entries.emplace_back(v.get("id", "").asString(),
-                             v.get("name", "").asString(),
-                             v.get("desc", "").asString(),
-                             v.get("url", "").asString());
-    }
-
-    return entries;
-}
+REGISTER_HTTP_SERVICE(Natur40Service, "natur40"); // NOLINT(cert-err58-cpp)
 
 void Natur40Service::run() {
     try {
@@ -141,11 +91,12 @@ void Natur40Service::run() {
                 try {
                     auto user = UserDB::createExternalUser(payload.get_claim_value<std::string>("sub"),
                                                            "JWT User",
-                                                           payload.get_claim_value<std::string>("sub"), externalId);
+                                                           payload.get_claim_value<std::string>("sub"),
+                                                           externalId);
 
                     session = UserDB::createSessionForExternalUser(externalId, 8 * 3600);
                 } catch (const std::exception &) {
-                    response.sendFailureJSON(concat("could not create new user "));
+                    response.sendFailureJSON("could not create new user ");
                     return;
                 }
             }
@@ -188,8 +139,7 @@ void Natur40Service::run() {
                 std::vector<CatalogEntry> entries = queryCatalog(jwt);
 
                 for (const CatalogEntry &e : entries) {
-                    const Json::Value value = e.toJson();
-                    result.append(value);
+                    result.append(e.toJson());
                 }
             } catch (UserDB::artifact_error &) {}
 
@@ -201,4 +151,70 @@ void Natur40Service::run() {
         response.sendFailureJSON(e.what());
     }
 
+}
+
+auto Natur40Service::CatalogEntry::toJson() const -> Json::Value {
+    Json::Value v(Json::ValueType::objectValue);
+    v["global_id"] = global_id;
+    v["title"] = title;
+    v["description"] = description;
+    v["user_url"] = user_url;
+
+    // TODO: add parameters that make the data instantiable for the frontend
+
+    return v;
+}
+
+auto Natur40Service::createCatalogSession(const std::string &token) const -> std::string {
+    cURL curl;
+    std::stringstream data;
+    curl.setOpt(CURLOPT_PROXY, Configuration::get<std::string>("proxy", "").c_str());
+
+    curl.setOpt(CURLOPT_URL, concat(Configuration::get<std::string>("natur40.catalog_auth_url"), token).c_str());
+    curl.setOpt(CURLOPT_WRITEFUNCTION, cURL::defaultWriteFunction);
+    curl.setOpt(CURLOPT_WRITEDATA, &data);
+    curl.setOpt(CURLOPT_COOKIEFILE, "");
+    curl.perform();
+
+    const std::vector<std::string> vector = curl.getCookies();
+
+    if (vector.empty()) {
+        throw NetworkException("Natur40 Catalog Cookie missing");
+    }
+
+    std::string cookie = vector[0];
+    return cookie.substr(cookie.rfind('\t') + 1);
+}
+
+auto Natur40Service::getCatalogJSON(const std::string &sessionId) const -> Json::Value {
+    cURL curl;
+    std::stringstream data;
+
+    curl.setOpt(CURLOPT_PROXY, Configuration::get<std::string>("proxy", "").c_str());
+
+    curl.setOpt(CURLOPT_URL, Configuration::get<std::string>("natur40.catalog_url").c_str());
+    curl.setOpt(CURLOPT_WRITEFUNCTION, cURL::defaultWriteFunction);
+    curl.setOpt(CURLOPT_COOKIE, concat("session=", sessionId, ";").c_str());
+    curl.setOpt(CURLOPT_WRITEDATA, &data);
+    curl.setOpt(CURLOPT_COOKIEFILE, "");
+    curl.perform();
+
+    Json::Reader reader(Json::Features::strictMode());
+    Json::Value entities;
+    if (!reader.parse(data.str(), entities))
+        throw std::runtime_error("Could not parse from Natur40 calatog");
+
+    return entities;
+}
+
+auto Natur40Service::queryCatalog(const std::string &token) -> std::vector<CatalogEntry> {
+    Json::Value json = getCatalogJSON(createCatalogSession(token));
+
+    std::vector<CatalogEntry> entries;
+
+    for (const Json::Value &v : json.get("entities", Json::Value(Json::ValueType::arrayValue))) {
+        entries.emplace_back(v);
+    }
+
+    return entries;
 }
